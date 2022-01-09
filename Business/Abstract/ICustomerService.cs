39f7e657967c24582a6fbb5555064484d27b79cf
OkhanT <m.okhan.tanriverdi@hotmail.com>
2022-01-10 02:41:27 +0300

    10.01.2022 - ödevin yarısı yapıldı-

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICustomerService
+    {
+        IDataResult<List<Customer>> GetAll();
+        IDataResult<List<Customer>> GetByUserId(int userId);
+        IDataResult<Customer> GetById(int customerId);
+        IDataResult<List<CustomerDetailDto>> GetCustomerDetailDto();
+
+        IResult Add(Customer customer);
+        IResult Delete(Customer customer);
+        IResult Update(Customer customer);
+
+
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,28 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, ReCapProjectContext>, ICustomerDal
+    {
+        public List<CustomerDetailDto> GetCustomerDetailDtos()
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var result = from u in context.Users
+                             join c in context.Customers
+                             on u.UserId equals c.UserId
+                             select new CustomerDetailDto {CustomerId=c.CustomerId,CompanyName=c.CompanyName,FirstName=u.FirstName,LastName=u.LastName,Email=u.Email,Password=u.Password };
+
+                return result.ToList();
+            }
+        }
+    }
+}
