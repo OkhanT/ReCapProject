2b455a192e680b882e2c69030a560f7731c8d6c8
OkhanT <m.okhan.tanriverdi@hotmail.com>
2022-01-24 01:02:15 +0300

    24.01.2022 - Autofac - FluentValidation - Aspect

--- Business/Concrete/RentalManegar.cs
+++ Business/Concrete/RentalManegar.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspect.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,7 +23,7 @@ namespace Business.Concrete
             _rentalDal = rentalDal;
         }
 
-        
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
             //ReturnDate yoksa araç kiralanmamıştır...
--- Business/ValidationRules/FluentValidation/RentalValidator.cs
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Business.Constants;
+using Entities.Concrete;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
