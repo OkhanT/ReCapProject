bb1651ce151d2e05d93904bffc6c9c6298c52df6
OkhanT <m.okhan.tanriverdi@hotmail.com>
2022-02-07 02:02:05 +0300

    07.02.2022 -Extensions -

--- Core/Utilities/Security/JWT/ITokenHelper.cs
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Core.Utilities.Security.JWT
 {
     public interface ITokenHelper
     {
-
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
     }
 }
